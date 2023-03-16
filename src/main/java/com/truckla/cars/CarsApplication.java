6ce4ccdc9cf6ffec1aed8e6ffdb5928d8d3a18fc
iKarmaaa123 <waael:hotmail.co.uk>
2023-03-16 15:47:46 +0000

    updates

--- src/main/java/com/truckla/cars/CarsApplication.java
+++ src/main/java/com/truckla/cars/CarsApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CarsApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Entering test");
 		SpringApplication.run(CarsApplication.class, args);
 	}
 
